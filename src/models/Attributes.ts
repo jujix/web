b270195525d7f5f1a2325c5942ab0dc5256a60a7
jujix <18301246+jujix@users.noreply.github.com>
2020-06-16 15:51:43 +0300

    Finalize composition approach

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,3 +1,5 @@
+import { UserProps } from "./User";
+
 export class Attributes<T> {
   constructor(private data: T) {}
 
@@ -7,5 +9,9 @@ export class Attributes<T> {
 
   set(update: T): void {
     Object.assign(this.data, update);
+  }
+
+  getAll(): T {
+    return this.data;
   }
 }
